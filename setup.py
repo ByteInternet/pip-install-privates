316524449ab1536c78d57f617ffc54890bc1860a
Andreas Lutro <andreas@byte.nl>
2018-04-16 13:48:08 +0200

    version 0.4

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name='pip_install_privates',
-    version='0.3',
+    version='0.4',
     description='Install pip packages from private repositories without an ssh agent',
     author='Byte Internet',
-    author_email='rickvandeloo@gmail.com',
+    author_email='tech@byte.nl',
     license='MIT',
     url='https://github.com/ByteInternet/pip_install_privates',
     packages=['pip_install_privates'],
