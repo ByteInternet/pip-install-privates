ef5341e0c753d68bbc33117a6f1c0216c2c5182c
Ben Konrath <ben@bagu.org>
2019-01-31 21:11:06 +0100

    Correct github url

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author='Byte Internet',
     author_email='tech@byte.nl',
     license='MIT',
-    url='https://github.com/ByteInternet/pip_install_privates',
+    url='https://github.com/ByteInternet/pip-install-privates',
     packages=['pip_install_privates'],
     install_requires=['pip'],
     entry_points={
